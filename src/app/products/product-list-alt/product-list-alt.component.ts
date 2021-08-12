43105889dbbbad3354165f289471a26941e3787a
sanjeev-gorijala <sanjeevg684@gmail.com>
2021-08-12 23:01:30 +0530

    APM Project

---
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { EMPTY, Subscription } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list-alt.component.html'
+})
+export class ProductListAltComponent  {
+  pageTitle = 'Products';
+  errorMessage = '';
+  selectedProductId: number;
+
+  products: Product[] = [];
+  sub: Subscription;
+
+  constructor(private productService: ProductService) { }
+
+  products$=this.productService.products$
+  .pipe(
+    catchError(err=>{
+      this.errorMessage=err;
+       return EMPTY;
+    }));
+
+  onSelected(productId: number): void {
+    console.log('Not yet implemented');
+  }
+}
